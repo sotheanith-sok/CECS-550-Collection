d69389d8aa2b2e5783c3993be5fb715ed5d66715
Sotheanith Sok <sotheanith@users.noreply.github.com>
2019-09-17 17:12:00 -0700

    add mean and std calculators

--- Project1/main.py
+++ Project1/main.py
@@ -2,15 +2,15 @@ import numpy as np
 
 
 class NaiveBayesianClassifer:
+
     def __init__(self):
         super().__init__()
-        print(self.loadData())
+        (self.x_train, self.y_train), (self.x_test, self.y_test) = self.loadData()
+        self.mean=self.getMean(self.x_train)
+        self.std=self.getStd(self.x_train)
+        self.y= np.count_nonzero(self.y_train==1)/self.y_train.size
 
-    """
-    Load data into program. 
-    """
     def loadData(self):
-        print("Loading data...")
         testSet = np.genfromtxt("./data/test.csv", delimiter=",")
         trainSet = np.genfromtxt("./data/train.csv", delimiter=",")
         x_train = trainSet[:, :8]
@@ -19,14 +19,38 @@ class NaiveBayesianClassifer:
         y_test = testSet[:, 8]
         return (x_train, y_train), (x_test, y_test)
 
-    def calculatesMean(self):
-        print("Calculating mean...")
+    def getMean(self, input):
+        shape = np.shape(input)
+        if len(shape) == 1:
+            result = np.ones(1)
+        else:
+            result = np.ones(shape[len(shape) - 1])
 
-    def calculateStandardDeviation(self):
-        print("Calculating standard devication...")
+        block = 1 if len(shape) == 1 else shape[len(shape) - 1]
 
-    def calculateNormalDistributionLikelihood(self):
-        print("Calculating Normal Distribution Likelihood...")
+        for i in range(block):
+            result[i] = np.var(input.flatten()[i::block])
+
+        return result
+
+    def getStd(self, input):
+        shape = np.shape(input)
+        if len(shape) == 1:
+            result = np.ones(1)
+        else:
+            result = np.ones(shape[len(shape) - 1])
+
+        block = 1 if len(shape) == 1 else shape[len(shape) - 1]
+
+        for i in range(block):
+            result[i] = np.std(input.flatten()[i::block])
+
+        return result
+
+
+    def getLikelihood(self, input):
+        print(input)
+            
 
     def classify(self):
         print("Classifying...")
@@ -36,4 +60,4 @@ class NaiveBayesianClassifer:
 
 
 nbc = NaiveBayesianClassifer()
-nbc.loadData()
+print(nbc.y)
