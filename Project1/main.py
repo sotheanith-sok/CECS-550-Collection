ece854ba9faabf2d3ccf1761402d1ff0d4efa99a
Sotheanith Sok <thetheanith@gmail.com>
2019-09-21 01:14:06 -0700

    Improve the classification and simplify the coding structure.

--- Project1/main.py
+++ Project1/main.py
@@ -1,130 +1,132 @@
 import numpy as np
-from sklearn.naive_bayes import GaussianNB
 
+class GaussianNaiveBayesianClassifier (object):
 
-class NaiveBayesianClassifer:
-    def __init__(self):
+    def __init__(self, shuffle=False, ratio=2.0/3.0):
+        """Initialize the object, load data, and start to predict the result.
+
+        Keyword Arguments:
+            shuffle {bool} -- Determine if training set and test set should be randomly pick from data set. Load predetermined training set and test set by default. (default: {False})
+            ratio {float} -- Determine the ratio between training set and test set. Only applicable if shuffle is true  (default: {2.0/3.0})
+        """
         super().__init__()
 
-        #Load data
-        self.train, self.test = self.loadData()
+        # Load data
+        self.trainingSet, self.testSet = self.loadData(shuffle)
 
-        #Dimension of X
-        numFeatures = 8 
+        # Calculate Prior probability
+        self.pY = np.count_nonzero(
+            self.trainingSet[:, 8]) / np.shape(self.trainingSet)[0]
 
-        #Calculate universal means, variances, and prior probability
-        self.mean = self.getMean(self.train)[:numFeatures]
-        self.var = self.getVar(self.train)[:numFeatures]
-        self.pY = [x[numFeatures] for x in self.train].count(1) / self.train.size
+        # Calculate likelihood of Y=1
+        self.meanPY, self.variancePY = self.calculateMeanAndVariance(
+            self.trainingSet[self.trainingSet[:, 8] == 1][:, :8])
 
-        #Calculate means and variances with respect to y=1
-        self.meanPY = self.getMean(self.train[self.train[:, numFeatures] == 1])[:numFeatures]
-        self.varPY = self.getVar(self.train[self.train[:, numFeatures] == 1])[:numFeatures]
+        # Calculate likelihood of Y=0
+        self.meanPNotY, self.VariancePNotY = self.calculateMeanAndVariance(
+            self.trainingSet[self.trainingSet[:, 8] == 0][:, :8])
 
-        #Calculate means and variance with respect to y=0
-        self.meanPNotY = self.getMean(self.train[self.train[:, numFeatures] == 0])[:numFeatures]
-        self.varPNotY = self.getVar(self.train[self.train[:, numFeatures] == 0])[:numFeatures]
+        # Initialize performance trackers to zero
+        self.TN, self.FP, self.FN, self.TP = 0, 0, 0, 0
 
-        #Points to keep track of models performance
-        self.TN = 0
-        self.FN = 0
-        self.FP = 0
-        self.TP = 0
+        # Start predicting on test set
+        self.predict()
 
-        #Starting testing
-        for data in self.test:
-            x = data[:8] #Input x of some dimension
-            y = data[8] #Expected output
+        # Calculate metrics and print them out
+        self.calculateMetrics()
 
-            #Calculate the three likelihood: universal, with respect to y=1, and with respect to y=2
-            l1, l2, l3 = (
-                self.getLikelihood(x, self.mean, self.var),
-                self.getLikelihood(x, self.meanPY, self.varPY),
-                self.getLikelihood(x, self.meanPNotY, self.varPNotY),
-            )
+    def loadData(self, shuffle=False, ratio=2.0/3.0):
+        """Load data into our object from files
 
-            #Classify the output.
-            predict = self.classify(l1, l2, l3, self.pY)
+        Keyword Arguments:
+            shuffle {bool} -- Determine if training set and test set should be randomly pick from data set. Load predetermined training set and test set by default. (default: {False})
+            ratio {float} -- Determine the ratio between training set and test set. Only applicable if shuffle is true  (default: {2.0/3.0})
+        """
+        # For non-shuffling data, use predetermine testing and training sets
+        if(shuffle == False):
+            testSet = np.genfromtxt("./data/test.csv", delimiter=",")
+            trainingSet = np.genfromtxt("./data/train.csv", delimiter=",")
+            return trainingSet, testSet
 
-            #Record the accuracy about predicted output vs expected output
-            if predict == 0 and y == 0:
-                self.TN = self.TN + 1
+        # Shuffle entire dataset and split base on ratio if required
+        else:
+            dataSet = np.genfromtxt(
+                "./data/pima-indians-diabetes.data.csv", delimiter=",")
+            np.random.shuffle(dataSet)
+            splitTarget = int(np.shape(dataSet)[0]*ratio)
+            return dataSet[:splitTarget], dataSet[splitTarget:]
 
-            if predict == 1 and y == 0:
-                self.FP = self.FP + 1
+    def calculateMeanAndVariance(self, input):
+        """Calculate means and variances column wise
 
-            if predict == 0 and y == 1:
-                self.FN = self.FN + 1
+        Arguments:
+            input {2d array} -- An array of features set
+        """
+        mean = np.mean(input, axis=0)
+        variance = np.var(input, axis=0)
+        return mean, variance
 
-            if predict == 1 and y == 1:
-                self.TP = self.TP + 1
+    def calculateGaussianPDF(self, x, mean, variance):
+        """Calcualte gaussian probability density of a single feature
 
-    '''
-    Load data from files
-    '''
-    def loadData(self):
-        testSet = np.genfromtxt("./data/test.csv", delimiter=",")
-        trainSet = np.genfromtxt("./data/train.csv", delimiter=",")
-        return trainSet, testSet
+        Arguments:
+            x {float} -- Value of a feature
+            mean {float} -- mean of other similar features
+            variance {float} -- variance of other similar features
 
-    '''
-    Generate means
-    '''
-    def getMean(self, input):
-        shape = np.shape(input)
-        result = np.ones(shape[len(shape) - 1])
-        block = shape[len(shape) - 1]
-        for i in range(block):
-            result[i] = np.mean(input.flatten()[i::block])
-        return result
+        Returns:
+            float -- Gaussian probability density
+        """
+        exponent = np.exp(-(np.power(x - mean, 2) / (2 * variance)))
+        return (1 / (np.sqrt(2 * np.pi * variance))) * exponent
 
-    '''
-    Generate variances
-    '''
-    def getVar(self, input):
-        shape = np.shape(input)
-        result = np.ones(shape[len(shape) - 1])
-        block = shape[len(shape) - 1]
-        for i in range(block):
-            result[i] = np.var(input.flatten()[i::block])
-        return result
+    def predict(self):
+        """
+        Predicts the test set and records results
+        """
+        for X in self.testSet:
+            probYX, probNotYX = 0, 0
+            for i in range(X.size-1):
 
-    '''
-    Calculate likelihood using multivariate gaussian distribution 
-    '''
-    def getLikelihood(self, x, mean, variance):
+                # Calculate P(X|Y=1)
+                probXY = self.calculateGaussianPDF(
+                    X[i], self.meanPY[i], self.variancePY[i])
+                if(probXY > 0):
+                    probYX = np.log(probXY)+probYX  # logarithmic estimation
 
-        #Find Σ
-        sigma = np.identity(x.size) * variance
+                # Calculate P(X|Y=0)
+                probXNotY = self.calculateGaussianPDF(
+                    X[i], self.meanPNotY[i], self.VariancePNotY[i])
+                if(probXNotY > 0):
+                    # logarithmic estimation
+                    probNotYX = np.log(probXNotY)+probNotYX
 
-        #Calculate the coefficient: 
-        coefficient = 1 / (
-            ((2 * np.pi) ** (x.size / 2)) * ((np.linalg.det(sigma)) ** (1 / 2))
-        )
+            # Calculate P(Y=1|X) = P(Y=1|X) * P(Y=1)
+            # P(Y=1|X) = log(P(Y=1|X) * P(Y=1)) = log(P(Y=1|X)) + log(P(Y=1))
+            probYX = probYX + np.log(self.pY)
 
-        #Calculate the exponent term
-        d = sigma ** (-1)
-        d[np.isinf(d)] = 0
-        a = np.atleast_2d(x - mean)  # 1 x n matrix
-        b = d  # n x n matrix
-        c = np.transpose(np.atleast_2d(x - mean))  # n x 1 matrix
+            # Calculate P(Y=0|X) = P(Y=0|X) * P(Y=0)
+            probNotYX = probNotYX + np.log(1-self.pY)
 
-        #Return the final results
-        return (coefficient * np.exp((-1 / 2) * np.linalg.multi_dot([a, b, c])))[0][0]
+            # Make the decision
+            predict = 1 if probYX > probNotYX else 0
 
-    '''
-    Classify the result
-    '''
-    def classify(self, likelihood1, likelihood2, likelihood3, pY):
-        return 1 if likelihood2 * pY >= likelihood3 * (1 - pY) else 0
+            # Record result
+            if predict == 0 and X[8] == 0:
+                self.TN = self.TN+1
+            elif predict == 0 and X[8] == 1:
+                self.FN = self.FN+1
+            elif predict == 1 and X[8] == 0:
+                self.FP = self.FP+1
+            else:
+                self.TP = self.TP+1
 
-    '''
-    Calcualte metrics
-    '''
-    def calculateAccuracy(self):
-        print((self.TP + self.TN) / (self.TN + self.TP + self.FN + self.FP))
+    def calculateMetrics(self):
+        """
+        Calcualte metrics such as accuracy and print out those matrics 
+        """
 
+        print((self.TP + self.TN) / (self.TP + self.FP + self.TN + self.FN))
 
 
-ndb = NaiveBayesianClassifer()
-ndb.calculateAccuracy()
+gnb = GaussianNaiveBayesianClassifier()
